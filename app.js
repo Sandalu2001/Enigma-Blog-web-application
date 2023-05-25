811208eccd45a1564c2f7429d061c1c3d5e2981e
Sandalu De Silva <86417299+Sandalu2001@users.noreply.github.com>
2023-05-25 20:36:09 +0530

    Add mongodb to the application
    remove all the arrays and used local mongodb database
    
    Have to connected to the mongodb atlas

--- app.js
+++ app.js
@@ -3,13 +3,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 // Load the full build.
 var lodash = require('lodash');
-
-let arr = [{
-  title : "Home",
-  data : "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.",
-  img : "https://img.freepik.com/free-photo/young-attractive-smiling-student-showing-thumb-up-outdoors-campus-university_8353-6394.jpg",
-}];
-
+const mongoose = require("mongoose");
 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
@@ -22,8 +16,40 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
+mongoose.connect('mongodb://localhost:27017/blogDB')
+  .then(()=>{
+    console.log("Mongodb has connected!")
+  })
+  .catch((err)=>{
+    console.log(err);
+  });
+
+
+//Create a imageSchema   
+const imageSchema = new mongoose.Schema({
+    name: String,
+    path: String,
+});
+  
+const Image = mongoose.model('Image', imageSchema);  
+
+
+//Create listSchema
+const listSchema = new mongoose.Schema({
+  title : String,
+  context : String,
+  image : String
+});   
+
+const List = mongoose.model("List",listSchema);
+
+//Home route
 app.get("/",function(req,res){
-  res.render("home",{contentArray : arr});
+
+  const data = List.find()
+    .then((data)=>{
+        res.render("home",{contentArray : data});
+    });
 });
 
 
@@ -47,38 +73,38 @@ app.get("/compose",function(req,res){
 app.get("/posts/:subject",function(req,res){
 
   let userInput = req.params.subject;
-  userInput = lodash.lowerCase(userInput);
-  
   console.log(userInput);
 
-  arr.forEach(function(post){
-    const storedTitle = lodash.lowerCase(post.title);
-    
-    if(storedTitle === userInput){
-      console.log("Match Found !");
-      res.render("post",{post : post});
-    }
-    else{
-      console.log("Match Not Found !");
-    }
-    
-  })
-})
+
+  const selectedPostList = List.find({title : userInput})
+    .then((selectedPostList)=>{
+      selectedPostList.forEach(function(item){
+        if(item.title === userInput){
+          console.log("Match Found !");
+          res.render("post",{post : item});
+        }
+        else{
+          console.log("Match Not Found !");
+          res.redirect("/");
+        }
+      });
+    })
+    .catch((err)=>{
+        console.log(err);
+    });
+});
 
 
 app.post("/compose",function(req,res){
   let img = req.body.avatar;
 
-  let dataObject = {
+  let item = new List({
     title : req.body.title,
-    data : req.body.data,
-    img : "https://picsum.photos/200/300",
-  }
+    context : req.body.data,
+    image : "https://picsum.photos/200/100"
+  });
 
-  arr.push(dataObject);
-
-
-  console.log(dataObject);
+  item.save();
 
   res.redirect("/");
 });
