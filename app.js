a15002056bd87340666a8cf88d7feef6a8a0da0b
Sandalu De Silva <86417299+Sandalu2001@users.noreply.github.com>
2023-05-26 21:51:23 +0530

    Deploy the application to the Internet and Change the appearance of the app

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
+//https://intense-mesa-98816.herokuapp.com
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 // Load the full build.
 var lodash = require('lodash');
 const mongoose = require("mongoose");
+const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
+require('dotenv').config();
+
 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
@@ -15,30 +22,37 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+var storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'public/uploads'); // Update the destination directory
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname)); // Include the original file extension
+  }
+});
 
-mongoose.connect('mongodb://localhost:27017/blogDB')
+var upload = multer({ storage: storage });
+
+
+
+mongoose.connect('mongodb+srv://0412233739St:0412233739St@cluster0.stkwziv.mongodb.net/blogDB?retryWrites=true&w=majority')
   .then(()=>{
     console.log("Mongodb has connected!")
   })
   .catch((err)=>{
     console.log(err);
   });
-
-
-//Create a imageSchema   
-const imageSchema = new mongoose.Schema({
-    name: String,
-    path: String,
-});
   
-const Image = mongoose.model('Image', imageSchema);  
 
 
 //Create listSchema
 const listSchema = new mongoose.Schema({
   title : String,
   context : String,
-  image : String
+  image : {
+    data: Buffer,
+    contentType: String
+  }
 });   
 
 const List = mongoose.model("List",listSchema);
@@ -95,21 +109,29 @@ app.get("/posts/:subject",function(req,res){
 });
 
 
-app.post("/compose",function(req,res){
-  let img = req.body.avatar;
-
-  let item = new List({
-    title : req.body.title,
-    context : req.body.data,
-    image : "https://picsum.photos/200/100"
+app.post("/compose", upload.single('image'), function(req, res) {
+  const item = new List({
+    title: req.body.title,
+    context: req.body.data
   });
 
-  item.save();
+  if (req.file) {
+    item.image.data = fs.readFileSync(req.file.path);
+    item.image.contentType = req.file.mimetype;
+  }
 
-  res.redirect("/");
+  item.save()
+    .then(() => {
+      res.redirect("/");
+    })
+    .catch((err) => {
+      console.log(err);
+      res.redirect("/");
+    });
 });
 
-app.listen(3000, function() {
+
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server started on port 3000");
 });
 
